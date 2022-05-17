b6430b2347ef95e62ae280c9adb55d44f6415a98
Ismael Doukoure <ismaeldoukoure.id@gmail.com>
2022-05-17 00:09:52 -0400

    add my first files to GitHub

---
+++ src/ca/uqam/inf2120/exemplesvusenclasse/classeobjet/tests/ProgrammePrincipal.java
@@ -0,0 +1,31 @@
+package ca.uqam.inf2120.exemplesvusenclasse.classeobjet.tests;
+
+import ca.uqam.inf2120.exemplesvusenclasse.classeobjet.Personne;
+
+
+/**
+ * Test de la classe Personne
+ * Date : Janvier 2014
+ * @author Ismael Doukoure
+ * @version 1.0
+ *
+ */
+public class ProgrammePrincipal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Créer et afficher l'objet personneUn
+		Personne personneUn = new Personne("Heraux", "Daniel", 101);
+		System.out.println( personneUn); //.afficher();
+		
+		// Créer et afficher l'objet personneDeux
+		Personne personneDeux = new Personne("Diané", "Mamady", 89);
+		personneDeux.afficher();
+		
+
+	}
+
+}
